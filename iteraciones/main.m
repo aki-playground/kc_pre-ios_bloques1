7a8ab9674af87bfee3619b3ca887c796efa6d59e
Akixe <akixe.otegi@gmail.com>
2016-02-22 14:29:43 +0100

    Initial Commit

---
+++ iteraciones/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  iteraciones
+//
+//  Created by Akixe on 22/2/16.
+//  Copyright © 2016 AOA. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
